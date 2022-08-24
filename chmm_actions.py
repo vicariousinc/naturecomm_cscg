66c81ea061314d85dd305cfc7deced5b573296dd
Miguel <miguel@vicarious.com>
2022-08-24 13:14:17 -0700

    Update chmm_actions.py

--- chmm_actions.py
+++ chmm_actions.py
@@ -1,23 +1,3 @@
-# ***********************************************************************
-#
-#     VICARIOUS
-#     _________
-#
-#      [2010] - [2022] Vicarious
-#      All Rights Reserved.
-#
-#     NOTICE:  All information contained herein is, and remains
-#     the property of Vicarious Incorporated and its suppliers,
-#     if any.  The intellectual and technical concepts contained
-#     herein are proprietary to Vicarious Incorporated
-#     and its suppliers and may be covered by U.S. and Foreign Patents,
-#     patents in process, and are protected by trade secret or copyright law.
-#     Dissemination of this information or reproduction of this material
-#     is strictly forbidden unless prior written permission is obtained
-#     from Vicarious Incorporated.
-#
-# ***********************************************************************
-
 from __future__ import print_function
 from builtins import range
 import numpy as np
